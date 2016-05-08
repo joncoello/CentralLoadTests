8ea0a70d6baa945daed6f8c05e382edcdce4146b
Jon Coello <joncoello@hotmail.com>
2016-05-08 20:27:56 +0100

    Add project files.

---
+++ CentralLoadTests/PlaybackTraceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+namespace CentralLoadTests {
+    [TestClass]
+    public class PlaybackTraceTests {
+        [TestMethod]
+        public void Playback() {
+
+            var traceContents = CentralLoadTests.Properties.Resources.login;
+            var document = XDocument.Parse(traceContents);
+
+            var query =
+                from el in document.Root.Descendants("Column")
+                where (string)el.Attribute("name") == "TextData"
+                select new TraceItem() {
+                    CommandText = el.Value
+                };
+
+            var results = query.ToList();
+
+        }
+    }
+}
